0acee86090600b21f8b4c465e572b455956ff5e5
ChadsLaptop <chadjcampbell@gmail.com>
2023-05-07 17:23:21 -0400

    fix bug in capitalize function

--- server/webHello.js
+++ server/webHello.js
@@ -7,7 +7,7 @@ function capitalize(string) {
   return string
     .trim()
     .toLowerCase()
-    .split()
+    .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
